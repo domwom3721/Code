a527cbcad0638f3e83eb64d646fd1c55a416b65b
Mike Leahy <michael.leahy@boweryvaluation.com>
2021-09-28 09:02:56 -0500

    Ready to delete all files but commented out DDB

--- General Code/Delete Empty Area and Hood Archvie Folders.py
+++ General Code/Delete Empty Area and Hood Archvie Folders.py
@@ -12,7 +12,6 @@ assert os.path.exists(hood_archive_root)
 
 
 for i in range(10):
-
     for (dirpath, dirnames, filenames) in os.walk(area_archive_root):
         if dirnames == [] and filenames == [] and dirpath != area_archive_root :
             print('Deleting ',dirpath)
@@ -24,7 +23,7 @@ for i in range(10):
             print('Deleting ',dirpath)
             os.rmdir(dirpath)
 
-#After we cleaned up the archvie folders, we can delete the files in the main area and hood folders so we can preserve the folder structure
+#After we cleaned up the archive folders, we can delete the files in the main area and hood folders so we can preserve the folder structure
 hood_root                   =  os.path.join(os.environ['USERPROFILE'], 'Dropbox (Bowery)','Research','Market Analysis','Neighborhood') 
 assert os.path.exists(hood_root)
 
@@ -35,14 +34,18 @@ assert os.path.exists(area_root)
 
 
 for (dirpath, dirnames, filenames) in os.walk(hood_root):
+    if 'Archive' in dirpath:
+        continue
     for file in filenames:
-        print(file)
+        print('Deleting',file)
         # os.remove(file)
 
 
 
 for (dirpath, dirnames, filenames) in os.walk(area_root):
+    if 'Archive' in dirpath:
+        continue
     for file in filenames:
-        print(file)
+        print('Deleting',file)
         # os.remove(file)
 
