5ca757640c6db5aec0b10a594fa96ca4b95194b8
Mike Leahy <michael.leahy@boweryvaluation.com>
2021-10-15 21:05:47 -0500

    commented out for saftey

--- General Code/Clean Archvie Folders.py
+++ General Code/Clean Archvie Folders.py
@@ -10,11 +10,11 @@ import os
 # hood_archive_root                   =  os.path.join(os.environ['USERPROFILE'], 'Dropbox (Bowery)','Research','Market Analysis','Neighborhood','Archive','2021') 
 # assert os.path.exists(hood_archive_root)
 
-market_archive_root                   =  os.path.join(os.environ['USERPROFILE'], 'Dropbox (Bowery)','Research','Market Analysis','Market','Archive','2021 Q2') 
-assert os.path.exists(market_archive_root)
+# market_archive_root                   =  os.path.join(os.environ['USERPROFILE'], 'Dropbox (Bowery)','Research','Market Analysis','Market','Archive','2021 Q2') 
+# assert os.path.exists(market_archive_root)
 
 
-for i in range(10):
+# for i in range(10):
     # for (dirpath, dirnames, filenames) in os.walk(area_archive_root):
     #     if dirnames == [] and filenames == [] and dirpath != area_archive_root :
     #         print('Deleting ',dirpath)
@@ -26,10 +26,10 @@ for i in range(10):
     #         print('Deleting ',dirpath)
     #         os.rmdir(dirpath)
     
-    for (dirpath, dirnames, filenames) in os.walk(market_archive_root):
-        if dirnames == [] and filenames == [] and dirpath != market_archive_root:
-            print('Deleting ',dirpath)
-            # os.rmdir(dirpath)
+    # for (dirpath, dirnames, filenames) in os.walk(market_archive_root):
+    #     if dirnames == [] and filenames == [] and dirpath != market_archive_root:
+    #         print('Deleting ',dirpath)
+    #         os.rmdir(dirpath)
 
 #After we cleaned up the archive folders, we can delete the files in the main area and hood folders so we can preserve the folder structure
 # hood_root                   =  os.path.join(os.environ['USERPROFILE'], 'Dropbox (Bowery)','Research','Market Analysis','Neighborhood') 
@@ -38,8 +38,8 @@ for i in range(10):
 # area_root                   =  os.path.join(os.environ['USERPROFILE'], 'Dropbox (Bowery)','Research','Market Analysis','Area') 
 # assert os.path.exists(area_root)
 
-market_root                   =  os.path.join(os.environ['USERPROFILE'], 'Dropbox (Bowery)','Research','Market Analysis','Market') 
-assert os.path.exists(market_root)
+# market_root                   =  os.path.join(os.environ['USERPROFILE'], 'Dropbox (Bowery)','Research','Market Analysis','Market') 
+# assert os.path.exists(market_root)
 
 
 
@@ -57,9 +57,11 @@ assert os.path.exists(market_root)
 #         print('Deleting',file)
 #         # os.remove(file)
 
-for (dirpath, dirnames, filenames) in os.walk(market_root):
-    if 'Archive' in dirpath:
-        continue
-    for file in filenames:
-        print('Deleting',file)
-        # os.remove(file)
+# for (dirpath, dirnames, filenames) in os.walk(market_root):
+#     if ('Archive'   in dirpath):
+#         continue
+#     for file in filenames:
+#         if ('2021 Q3' in file):
+#             continue
+#         print('Deleting',os.path.join(dirpath,file))
+#         os.remove(os.path.join(dirpath,file))
