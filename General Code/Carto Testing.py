c6ba0df279143f6a317526a86026dab2cb9f1106
Dominic Toth <dominic.toth@boweryvaluation.com>
2021-11-19 17:15:21 -0500

    delete

--- General Code/Carto Testing.py
+++ General Code/Carto Testing.py
@@ -10,3 +10,14 @@ local_file_or_URL = ""
 
 dataset_manager = DatasetManager(auth_client)
 dataset = dataset_manager.create(LOCAL_FILE_OR_URL)
+
+# return the id of the sync
+#sync_id = syncTable.get_id()
+
+#while(syncTable.state != 'success'):
+#    time.sleep(5)
+#    syncTable.refresh()
+#    if (syncTable.state == 'failure'):
+#        print('The error code is: ' + str(syncTable.error_code))
+#        print('The error message is: ' + str(syncTable.error_message))
+#        break
