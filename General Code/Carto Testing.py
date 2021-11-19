14d279b9ccf7335d0eb17123b5c4e2191fb853a9
Dominic Toth <dominic.toth@boweryvaluation.com>
2021-11-19 17:15:47 -0500

    mapmanager

--- General Code/Carto Testing.py
+++ General Code/Carto Testing.py
@@ -1,10 +1,13 @@
+from carto.datasets import DatasetManager
+from carto.maps import NamedMapManager, NamedMap
+import json
+
 from carto.auth import APIKeyAuthClient
 
 USERNAME="boweryres"
 USR_BASE_URL = "https://{user}.carto.com/".format(user=USERNAME)
 auth_client = APIKeyAuthClient(api_key="665cd4b5ebe654dad7101dcf0048718f45282835", base_url=USR_BASE_URL)
 
-from carto.datasets import DatasetManager
 #local file or url
 local_file_or_URL = ""
 
@@ -12,12 +15,4 @@ dataset_manager = DatasetManager(auth_client)
 dataset = dataset_manager.create(LOCAL_FILE_OR_URL)
 
 # return the id of the sync
-#sync_id = syncTable.get_id()
-
-#while(syncTable.state != 'success'):
-#    time.sleep(5)
-#    syncTable.refresh()
-#    if (syncTable.state == 'failure'):
-#        print('The error code is: ' + str(syncTable.error_code))
-#        print('The error message is: ' + str(syncTable.error_message))
-#        break
+sync_id = syncTable.get_id()
