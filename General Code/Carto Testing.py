22b8e59f962690ff80b0c53afd1b7a26661ef371
Dominic Toth <dominic.toth@boweryvaluation.com>
2021-11-19 17:06:00 -0500

    dataset manager

--- General Code/Carto Testing.py
+++ General Code/Carto Testing.py
@@ -3,3 +3,10 @@ from carto.auth import APIKeyAuthClient
 USERNAME="boweryres"
 USR_BASE_URL = "https://{user}.carto.com/".format(user=USERNAME)
 auth_client = APIKeyAuthClient(api_key="665cd4b5ebe654dad7101dcf0048718f45282835", base_url=USR_BASE_URL)
+
+from carto.datasets import DatasetManager
+#local file or url
+local_file_or_URL = ""
+
+dataset_manager = DatasetManager(auth_client)
+dataset = dataset_manager.create(LOCAL_FILE_OR_URL)
