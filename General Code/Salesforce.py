cdaf877a45d50d6ece28a3027b93f99bb152fd7e
Dominic Toth <dominic.toth@boweryvaluation.com>
2021-12-07 16:15:38 -0500

    salesforce

--- General Code/Salesforce.py
+++ General Code/Salesforce.py
@@ -1,8 +1,15 @@
-from simple_salesforce import Salesforce
+from simple_salesforce import Salesforce, SalesforceLogin, SFType
 import requests
+import json
 import pandas as pd
 from io import StringIO
 
 sf = Salesforce(username='dominic.toth@boweryvaluation.com',password='DOMcathy80!@',security_token='977pcdtBN8m5VTRz1FfSzi4hG')
 #login for Mike
 #sf = Salesforce(username='mike.leahy@boweryvaluation.com',password='',security_token='')
+
+sf = Salesforce(instance_url='https://boweryvaluation.lightning.force.com', session_id='00O4X000009MgMOUA0')
+
+#get https://boweryvaluation.my.salesforce.com/services/data/vXX.X/resource/
+
+sf.Contact.create({'LastName':'Smith','Email':'example@example.com'})
