84dbf1f0dfeca9cf351a302ae7fcc8f9c6108396
Dominic Toth <dominic.toth@boweryvaluation.com>
2021-12-08 09:55:17 -0500

    salesforce_API

--- General Code/Salesforce.py
+++ General Code/Salesforce.py
@@ -1,5 +1,9 @@
+#Salesforce API used to systematically identify new jobs, their area and hood, and create an area and report
+#https://ryanwingate.com/salesforce/salesforce-and-python/programmatically-obtaining-all-fields/
+
 from unicodedata import name
 from numpy import where
+from numpy.lib.function_base import select
 from simple_salesforce import Salesforce, SalesforceLogin, SFType
 import requests
 import json
@@ -9,19 +13,51 @@ from io import StringIO
 sf = Salesforce (
     username = 'dominic.toth@boweryvaluation.com',
     password = 'DOMcathy80!@',
-    security_token = '977pcdtBN8m5VTRz1FfSzi4hG'
+    security_token = '977pcdtBN8m5VTRz1FfSzi4hG',
+    client_id = 'boweryvaluation',
+    #sandbox = True
 )
 
-desc = sf.Account.describe()  
+desc = sf.describe()
+print(desc)
 
-field_names = [field['name'] for field in desc['fields']]
+desc = sf.describe()
+objects = []
+obj_labels = [obj['label']  for obj in desc['sobjects']]
+obj_names  = [obj['name']   for obj in desc['sobjects']]
+obj_custom = [obj['custom'] for obj in desc['sobjects']]
+for label, name, custom in zip(obj_labels, obj_names, obj_custom):
+    objects.append((label, name, custom))
+objects = pd.DataFrame(objects,
+                       columns = ['label','name','custom'])
+print(str(objects.shape[0]) + ' objects')
+objects.head(10)
 
-sf_data = sf.query_all("SELECT Address, Market_Research_Due_Date__c, FROM Market_Research WHERE assigned_To__c IN ('D')")
+#this example uses the Job object
+desc = sf.Job_c.describe()
+fields = []
+field_labels = [field['label'] for field in desc['fields']]
+field_names =  [field['name']  for field in desc['fields']]
+field_types =  [field['type']  for field in desc['fields']]
+for label, name, dtype in zip(field_labels, field_names, field_types):
+    fields.append((label, name, dtype))
+fields = pd.DataFrame(fields,
+                      columns = ['label','name','type'])
+print(str(fields.shape[0]) + ' fields')
+fields.head(10)
 
-print(sf_df)
 
 
+#field_names = [field['name'] for field in desc['fields']]
+#sf_data = sf.query_all("SELECT Address, Market_Research_Due_Date__c, Assigned_To_c")
+#sf_df = pd.DataFrame(sf_data['records']).drop(columns='attributes')
 
+#print(sf_df)
+
+#field_names = [field['name'] for field in desc['fields']]
+#soql = "SELECT {} FROM Account".format(','.join(field_names))
+#results = sf.query_all(soql) #normal download
+#results = sf.bulk.Account.query(soql) #bulk download
 
 
 
