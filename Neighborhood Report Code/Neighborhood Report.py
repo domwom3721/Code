43804a678028a653923d1c2f49bf0ee86b222812
Mike Leahy <michael.leahy@boweryvaluation.com>
2021-12-21 13:37:36 -0600

    report file name lenght limit

--- Neighborhood Report Code/Neighborhood Report.py
+++ Neighborhood Report Code/Neighborhood Report.py
@@ -378,7 +378,6 @@ def CreateDirectory():
         hood_folder              = os.path.join(main_output_location,hood_state,neighborhood)
         hood_folder_map          = os.path.join(map_location,hood_state,neighborhood)
     
-    report_path = os.path.join(hood_folder,current_year + ' ' + hood_state + ' - ' + neighborhood  + ' - hood' + '_draft.docx')
 
 
 
@@ -387,7 +386,8 @@ def CreateDirectory():
             pass 
          else:
             os.mkdir(folder) 
-
+    report_path = os.path.join(hood_folder,current_year + ' ' + hood_state + ' - ' + neighborhood  + ' - hood' + '_draft.docx')
+    report_path = report_path[:255]
 def FindZipCodeDictionary(zip_code_data_dictionary_list,zcta,state_fips):
     #This function takes a list of dictionaries, where each zip code gets its own dictionary. Takes a zip code and state fips code and finds and returns just that dictionary.
     #We need to use this, because the census api is causing an error that requires us to retrive data for all zip codes in the country
