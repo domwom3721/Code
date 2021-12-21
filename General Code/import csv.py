07bdb7b0c0993c55e41dbb19d2d3c642566fdd26
Mike Leahy <michael.leahy@boweryvaluation.com>
2021-12-21 09:07:01 -0600

    dom changes

--- General Code/import csv.py
+++ General Code/import csv.py
@@ -1,5 +1,6 @@
 import csv
 import os
+from numpy import float64
 import pandas as pd
 from pandas import read_csv
 from pandas.core.frame import DataFrame
@@ -14,8 +15,10 @@ daily_salesforce_jobs          =  os.path.join(salesforce_report,'report.csv')
 # Import raw census places data as pandas data frames
 open(daily_salesforce_jobs,'rb')
 df_salesforce = pd.read_csv(daily_salesforce_jobs)
-print(df_salesforce)
-
-#df_salesforce["latlong"] = df_salesforce['Property Latitude', 'Property Longitude'].agg(','.join)
-#df_salesforce["lat_long"] = list(zip(df_salesforce["property_latitude"], df_salesforce["property_longitude"]))
 #print(df_salesforce)
+df = pd.DataFrame(df_salesforce)
+#print (df.dtypes)
+
+df['lat_long'] = df["Property Latitude"].astype(str) + ',' + df['Property Longitude'].astype(str)
+df.head()
+print(df_salesforce)
