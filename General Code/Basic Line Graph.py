fe7f09678d0e601941dfeac57e56f700b1b940fb
Mike Leahy <michael.leahy@boweryvaluation.com>
2022-01-18 08:53:54 -0600

    lg change

--- General Code/Basic Line Graph.py
+++ General Code/Basic Line Graph.py
@@ -10,11 +10,11 @@ from plotly.subplots import make_subplots
 
 #Define file path of data file
 desktop_location = os.path.join(os.environ['USERPROFILE'],'Desktop')
-file_location    = os.path.join(desktop_location,'CPIAUCSL.csv') 
+file_location    = os.path.join(desktop_location,'UNRATE.csv') 
 
 #Import data folder
 df  = pd.read_csv(file_location)
-df["Monthly % Change"] = ((df['CPIAUCSL']/df['CPIAUCSL'].shift(1)) - 1 ) * 100
+# df["Monthly % Change"] = ((df['CPIAUCSL']/df['CPIAUCSL'].shift(1)) - 1 ) * 100
 
 
 
@@ -52,34 +52,37 @@ title_position = .95
 
 
 #Create Graph
-fig = make_subplots(specs=[[{"secondary_y": True}]])
+fig = make_subplots(specs=[[{"secondary_y": False}]])
 
 # Add line 
 fig.add_trace(
 go.Scatter(x=df['DATE'],
-        y=df['CPIAUCSL'],
-        name='Consumer Price Index (R)',
+        y=df['UNRATE'],
+        name='National Unemployment Rate',
         line=dict(color="#4160D3", width=3),
         mode = 'lines'
         )
-,secondary_y=True)  
+        )  
 
-#Add Bars
-fig.add_trace(
-go.Bar(x=df['DATE'],
-        y=df["Monthly % Change"],
-        name="Monthly % Change (L)",
-        marker_color="#D7DEEA")
-        ,secondary_y=False
-        )
+# #Add Bars
+# fig.add_trace(
+# go.Bar(x=df['DATE'],
+#         y=df["Monthly % Change"],
+#         name="Monthly % Change (L)",
+#         marker_color="#D7DEEA")
+#         ,secondary_y=False
+#         )
+
+
 
 #Update y-axis
-fig.update_yaxes(tickfont = dict(size=tickfont_size), ticksuffix = '%',  title = None, secondary_y=False)                
+fig.update_yaxes(tickfont = dict(size=tickfont_size), ticksuffix = '%',  title = None, )   
+fig.update_xaxes(tickfont = dict(size=tickfont_size), title = None,tickmode = 'auto',nticks =20 )   
 
 
 #Set formatting 
 fig.update_layout(
-title_text="Consumer Price Index and Monthly % Change",    
+title_text="National Unemployment Rate",    
 title={
     'y':title_position,
     'x':0.5,
