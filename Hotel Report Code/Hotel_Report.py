23fdf43ea9c9b9c58a31a596537cdc622fa1cafc
Mike Leahy <michael.leahy@boweryvaluation.com>
2022-01-19 17:23:18 -0600

    cd chagne

--- Hotel Report Code/Hotel_Report.py
+++ Hotel Report Code/Hotel_Report.py
@@ -17,9 +17,9 @@ from docx.shared import Inches, Pt, RGBColor
 
 #Define file pre paths
 dropbox_root                   =  os.path.join(os.environ['USERPROFILE'], 'Dropbox (Bowery)') 
-# project_location               =  os.path.join(dropbox_root,'Research','Projects','Research Report Automation Project')      #Main Folder that stores all output, code, and documentation
+project_location               =  os.path.join(dropbox_root,'Research','Projects','Research Report Automation Project')      #Main Folder that stores all output, code, and documentation
 output_location                = os.path.join(dropbox_root,'Research','Market Analysis','Market','Other','Hotel')           #The folder where we store our current reports, production
-output_location                = os.path.join(project_location,'Output','Hotel')                                              #The folder where we store our current reports, testing folder
+# output_location                = os.path.join(project_location,'Output','Hotel')                                              #The folder where we store our current reports, testing folder
 map_location                   = os.path.join(project_location,'Data','Hotel Reports Data','Hotel Maps')                     #Folders with maps png files  
 general_data_location          =  os.path.join(project_location,'Data','General Data')                                        #Folder with data used in multiple report types
 hotel_data_location            = os.path.join(project_location,'Data','Hotel Reports Data')                                   #Folder with data for hotel reports only
