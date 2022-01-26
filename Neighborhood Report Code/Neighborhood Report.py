542c3886e0f16680ea6f9c435dcfec660a421151
Mike Leahy <michael.leahy@boweryvaluation.com>
2022-01-26 10:22:51 -0600

    typo

--- Neighborhood Report Code/Neighborhood Report.py
+++ Neighborhood Report Code/Neighborhood Report.py
@@ -3832,7 +3832,7 @@ def TravelMethodLanguage():
 
 
 
-        travel_method_language = ('In ' + neighborhood + ', the majority of residents' + hood_largest_travel_category.lower()  + ' with ' +  "{:,.0f}%".format(hood_largest_travel_category_frac) + ' choosing to do so.' +
+        travel_method_language = ('In ' + neighborhood + ', the majority of residents ' + hood_largest_travel_category.lower()  + ' with ' +  "{:,.0f}%".format(hood_largest_travel_category_frac) + ' choosing to do so.' +
                                  ' The second most popular method is ' + 
                                  second_most_common_category +
                                  ' with ' +
