4a430a2d72065ceda01c5765931c284ff9dcd4be
Mike Leahy <michael.leahy@boweryvaluation.com>
2022-02-02 16:47:21 -0600

    code folder reorg

---
+++ General Code/Report Tracker.py
@@ -0,0 +1,7 @@
+#Date: 02/02/2022
+#Author: Mike Leahy
+#Summary: Uses our 3 report csv summary files and produces a summary of jobs produced by each member of the research team
+
+import os
+import pandas as pd
+
