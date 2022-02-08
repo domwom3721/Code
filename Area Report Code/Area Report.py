b69766b3d748c1cc442b717105891fdc4b8a946f
Mike Leahy <michael.leahy@boweryvaluation.com>
2022-02-08 14:45:06 -0600

    formatting

--- Area Report Code/Area Report.py
+++ Area Report Code/Area Report.py
@@ -5456,6 +5456,7 @@ def AddMap(document):
             except:
                 pass
     Citation(document=document,text = 'Google Maps')
+    
 def AddTwoColumnTable(document,pic_list,lang_list):
     #Insert the transit graphics(car, bus,plane, train)
     tab = document.add_table(rows=0, cols=2)
