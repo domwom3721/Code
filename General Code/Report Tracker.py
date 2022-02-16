9f53202dd32e0b121b02455791656d73f9c2998e
Mike Leahy <michael.leahy@boweryvaluation.com>
2022-02-16 15:31:21 -0600

    KPI Scrpit indent

--- General Code/Report Tracker.py
+++ General Code/Report Tracker.py
@@ -6,18 +6,18 @@ import os
 import pandas as pd
 
 #Define file paths
-dropbox_root                   =  os.path.join(os.environ['USERPROFILE'], 'Dropbox (Bowery)') 
-main_output_location           =  os.path.join(dropbox_root,'Research','Market Analysis') 
+dropbox_root                        =  os.path.join(os.environ['USERPROFILE'], 'Dropbox (Bowery)') 
+main_output_location                =  os.path.join(dropbox_root,'Research','Market Analysis') 
 
 #Read in our 3 csv reports as dataframes
-area_report_df    = pd.read_csv(os.path.join(main_output_location,'Area','Dropbox Areas.csv'),        encoding='latin-1')
-market_report_df  = pd.read_csv(os.path.join(main_output_location,'Market','CoStar Markets.csv'),     encoding='latin-1')
-hood_report_df    = pd.read_csv(os.path.join(main_output_location,'Neighborhood','Dropbox Neighborhoods.csv'),encoding='latin-1')
+area_report_df                      = pd.read_csv(os.path.join(main_output_location,'Area','Dropbox Areas.csv'),        encoding='latin-1')
+market_report_df                    = pd.read_csv(os.path.join(main_output_location,'Market','CoStar Markets.csv'),     encoding='latin-1')
+hood_report_df                      = pd.read_csv(os.path.join(main_output_location,'Neighborhood','Dropbox Neighborhoods.csv'),encoding='latin-1')
 
 #Restrict to Final Reports
-area_report_df    = area_report_df.loc[area_report_df['Status'] == 'Final']
-market_report_df  = market_report_df.loc[market_report_df['Status'] == 'Final']
-hood_report_df    = hood_report_df.loc[hood_report_df['Status'] == 'Final']
+area_report_df                       = area_report_df.loc[area_report_df['Status'] == 'Final']
+market_report_df                     = market_report_df.loc[market_report_df['Status'] == 'Final']
+hood_report_df                       = hood_report_df.loc[hood_report_df['Status'] == 'Final']
 
 
 #Collapse down each dataframe to the total done by each team member
@@ -44,32 +44,3 @@ kpi_df.to_csv(os.path.join(main_output_location,'KPI.csv'),index=False)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-# #Rename verion variables
-# area_report_df                      = area_report_df.rename(columns={"Version": "Area Report Version"})
-# market_report_df                    = market_report_df.rename(columns={"Version": "Market Report Version"})
-# hood_report_df                      = hood_report_df.rename(columns={"Version": "Hood Report Version"})
-
-# #Restrict to latest version
-# latest_market_verion = '2021 Q4'
-# latest_area_verion   = '2021 Q4'
-# latest_hood_version  = 2022
-
-
-# area_report_df    = area_report_df.loc[area_report_df['Version']     == latest_area_verion ]
-# market_report_df  = market_report_df.loc[market_report_df['Version'] == latest_market_verion]
-# hood_report_df    = hood_report_df.loc[hood_report_df['Version']     == latest_hood_version]
-
-#Append the 3 dataframes together
-# kpi_df                              = pd.merge(market_report_df,area_report_df, on=['Assigned To'],how = 'left') 
-# kpi_df                              = pd.merge(kpi_df,hood_report_df, on=['Assigned To'],how = 'left') 
