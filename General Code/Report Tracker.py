6b2fcf90b84775707df54bdabf1c7be0b897d2ab
Mike Leahy <michael.leahy@boweryvaluation.com>
2022-02-22 17:01:38 -0600

    directory

--- General Code/Report Tracker.py
+++ General Code/Report Tracker.py
@@ -8,6 +8,7 @@ import pandas as pd
 #Define file paths
 dropbox_root                        =  os.path.join(os.environ['USERPROFILE'], 'Dropbox (Bowery)') 
 main_output_location                =  os.path.join(dropbox_root,'Research','Market Analysis') 
+kpi_location                        =  os.path.join(dropbox_root,'Research','Projects','KPI') 
 
 #Read in our 3 csv reports as dataframes
 area_report_df                      = pd.read_csv(os.path.join(main_output_location,'Area','Dropbox Areas.csv'),        encoding='latin-1')
@@ -40,7 +41,7 @@ kpi_df                              = kpi_df.sort_values(by=['Type','Version'])
 
 print(kpi_df)
 #Export as csv file
-kpi_df.to_csv(os.path.join(dropbox_root,'Research','Projects','KPI','KPI.csv'),index=False)
+kpi_df.to_csv(os.path.join(kpi_location,'KPI.csv'),index=False)
 
 
 
