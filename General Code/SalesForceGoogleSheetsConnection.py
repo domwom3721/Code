eb909fbcbb8d4b12db5fb4c17eac5e2ce4d37dba
Mike Leahy <michael.leahy@boweryvaluation.com>
2022-02-24 09:37:34 -0600

    SF connection working

--- General Code/SalesForceGoogleSheetsConnection.py
+++ General Code/SalesForceGoogleSheetsConnection.py
@@ -5,5 +5,6 @@ import pandas as pd
 
 r = requests.get('https://docs.google.com/spreadsheet/ccc?key=1fIP8dwH5hwSDMKEmOUdbnvwbwMZyAOClAm_4HDVVe5k&output=csv')
 data = r.content
-df = pd.read_csv(BytesIO(data), index_col=0)
+df = pd.read_csv(BytesIO(data),engine='python')
 print(df)
+
