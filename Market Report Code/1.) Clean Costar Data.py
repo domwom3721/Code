d6ce1e1e8b5e37baf754a3a7cfefa164d495c6ed
Dominic Toth <dominic.toth@boweryvaluation.com>
2022-07-07 15:20:37 -0400

    datafilepath_and_printing

--- Market Report Code/1.) Clean Costar Data.py
+++ Market Report Code/1.) Clean Costar Data.py
@@ -16,10 +16,10 @@ raw_office_file                =  os.path.join(costar_data_location,'Raw Data','
 raw_retail_file                =  os.path.join(costar_data_location,'Raw Data','retail.csv') 
 raw_industrial_file            =  os.path.join(costar_data_location,'Raw Data','industrial.csv') 
 
-raw_multifamily_slices_file    =  os.path.join(costar_data_location,'Raw Data','mf_slices.xlsx') 
+raw_multifamily_slices_file    =  os.path.join(costar_data_location,'Raw Data','mf_slices.csv') 
 raw_office_slices_file         =  os.path.join(costar_data_location,'Raw Data','office_slices.xlsx') 
-raw_retail_slices_file         =  os.path.join(costar_data_location,'Raw Data','retail_slices.csv') 
-raw_industrial_slices_file     =  os.path.join(costar_data_location,'Raw Data','industrial_slices.xlsx') 
+raw_retail_slices_file         =  os.path.join(costar_data_location,'Raw Data','retail_slices.xlsx') 
+raw_industrial_slices_file     =  os.path.join(costar_data_location,'Raw Data','industrial_slices.csv') 
 
 
 #Import raw CoStar data as pandas dataframes
@@ -55,7 +55,7 @@ df_office       = pd.read_csv(raw_office_file,
                         'Market Rent Growth':           object,
                         'Market Rent Growth 12 Mo':     object,
                         'Available SF':                 object,
-                        'Vacacny Rate':                 object,
+                        'Vacancy Rate':                 object,
                         'Median Cap Rate':              object,
                         'Median Price/Bldg SF':         object,
                         'Demand SF':                    object,
@@ -87,9 +87,11 @@ df_industrial   = pd.read_csv(raw_industrial_file,
                         }
                              )  		
 
+print('Importing Raw CoStar Data')
+
 #Import the raw slices data from Costar where the markets are broken down by the quality or type of the properties
 if os.path.exists(raw_multifamily_slices_file):
-    df_multifamily_slices  = pd.read_excel(raw_multifamily_slices_file,
+    df_multifamily_slices  = pd.read_csv(raw_multifamily_slices_file,
                     dtype={'Sales Volume Transactions': object,
                           'Market Effective Rent/Unit': float,
                           'Inventory Units': int64
@@ -108,7 +110,7 @@ if os.path.exists(raw_office_slices_file):
                                         )
 
 if os.path.exists(raw_retail_slices_file):
-    df_retail_slices       = pd.read_csv(raw_retail_slices_file,
+    df_retail_slices       = pd.read_excel(raw_retail_slices_file,
                     dtype={'Sales Volume Transactions':  object,
                         'Cap Rate Transactions':        object,
                         'Gross Delivered Buildings':    object,
@@ -124,7 +126,7 @@ if os.path.exists(raw_retail_slices_file):
                                         )
 
 if os.path.exists(raw_industrial_slices_file):
-    df_industrial_slices   = pd.read_excel(raw_industrial_slices_file,
+    df_industrial_slices   = pd.read_csv(raw_industrial_slices_file,
                     dtype={'Sales Volume Transactions':    object,
                             'Sold Building SF':             object,
                             'Total Sales Volume':           object,
@@ -135,42 +137,58 @@ if os.path.exists(raw_industrial_slices_file):
                             }
                                         )
 
+print('Importing Raw CoStar Sliced Data')
+
 #Define data cleaning functions
 def DropClusters(df): 
     #Drops rows that report data on the cluster geography type
     df = df.loc[df['Geography Type'] != 'Cluster']
     return(df)
+print('Dropping Clusters')
 
 def DropUrban(df): 
     #Drops rows that report data on the urban geography type
     df = df.loc[df['Geography Type'] != 'Location Type:Urban']
     return(df)
 
+print('Dropping Urban')
+
+
 def DropSuburban(df): 
     #Drops rows that report data on the suburban geography type
     df = df.loc[df['Geography Type'] != 'Location Type:Suburban']
     return(df)
+
+print('Dropping Suburban')
 
 def DropCBD(df): 
     #Drops rows that report data on the cbd geography type
     df = df.loc[df['Geography Type'] != 'Location Type:CBD']
     return(df)
 
+print('Dropping CBD')
+
 def KeepLast10Years(df,groupbylist): 
     #Cut down to last 10 years
     df = df.groupby(groupbylist).tail(40)
     return(df)
+
+print('Keeping Last 10 Years')
 
 def SortData(df):
      #Sorts by geography and quarter
     df = df.sort_values(by=['Geography Name','Period'])
     return(df)
 
+print('Sorting Data')
+
 def CreateYearAndQuarterVariables(df): 
     #Seperates the period variable into 2 components (year and quarter)
     df.loc[:,'Year']           =   df.loc[:,'Period'].str[:4]
     df.loc[:,'Quarter']        =   df.loc[:,'Period'].str[5:]
     return(df)
+
+print('Creating Year & Quarter Variables')
 
 def DropExtraVariables(df,sector): 
     #Drops the variables we don't use in our analysis
@@ -186,6 +204,8 @@ def DropExtraVariables(df,sector):
                              ]
                     )
     return(df)
+
+print('Dropping Extra Variables')
 
 def DestringVariablesConvertToNumeric(df,sector):
 
@@ -275,9 +295,9 @@ def ConvertPercenttoPercentagePoints(df,sector):
     else:
         rate_vars = ['Vacancy Rate',
                     'Availability Rate',
-                    'Market Cap Rate',
-                   'Market Rent Growth',
-                   'Market Rent Growth 12 Mo' ]
+                    'Market Cap Rate', ]
+                   #'Market Rent Growth', 
+                   #'Market Rent Growth 12 Mo' 
     
     for var in rate_vars:
             df[var] = round(df[var] * 100,4)
@@ -294,9 +314,9 @@ def MainClean(df,sector): #Calls all cleaning functions and returns cleaned data
 
 def MainCleanSlices(df,sector): #Calls cleaning functions and returns cleaned dataframes for our sliced data
     if sector == 'Multifamily':
-        df = df[['Property Class Name','Period','Slice','As Of','Geography Name','Property Type','Vacancy Rate','Market Effective Rent/Unit','Inventory Units']]
+        df = df[['Property Class Name','Period','Slice','As Of','Geography Name','Property Type','Vacancy Rate','Market Effective Rent/Unit','Market Effective Rent Growth','Market Effective Rent Growth 12 Mo','Inventory Units']]
     else:
-        df = df[['Property Class Name','Period','Slice','As Of','Geography Name','Property Type','Vacancy Rate','Market Rent/SF','Inventory SF','Under Construction SF','Net Delivered SF 12 Mo','Availability Rate','Net Absorption SF 12 Mo']]
+        df = df[['Property Class Name','Period','Slice','As Of','Geography Name','Property Type','Vacancy Rate','Market Rent/SF','Market Rent Growth','Market Rent Growth 12 Mo','Inventory SF','Under Construction SF','Net Delivered SF 12 Mo','Availability Rate','Net Absorption SF 12 Mo']]
         df['Availability Rate'] = df['Availability Rate'] * 100
 
         #Replace NA with 0
@@ -328,7 +348,7 @@ def MainCleanSlices(df,sector): #Calls cleaning functions and returns cleaned da
             
     #Remove "Center" from slice name
     df['Slice']                = df['Slice'].str.replace(' Center', '', regex=False)
-    df['Geography Name']       = df['Geography Name'].str.replace('New York City', 'Manhattan', regex=False)
+    #df['Geography Name']       = df['Geography Name'].str.replace('New York City', 'Manhattan', regex=False)
     
     #Drop the aggregate slice
     df                         =   df.loc[df['Slice'] != 'All']
@@ -348,12 +368,12 @@ df_office_slices        =  MainCleanSlices(df_office_slices,'Office')
 df_retail_slices        =  MainCleanSlices(df_retail_slices,'Retail')
 df_industrial_slices    =  MainCleanSlices(df_industrial_slices,'Industrial')
 
-
+print('Cleaning Sliced Data')
 
 #Loop through the 4 dataframes: create variables we will use in our report/figures 
 for df in [df_multifamily,df_office,df_retail,df_industrial]:
 
-    df['Geography Name'] = df['Geography Name'].str.replace('New York City', 'Manhattan', regex=False)
+    #df['Geography Name'] = df['Geography Name'].str.replace('New York City', 'Manhattan', regex=False)
 
     #Clean the sqft and unit variables seperately
     if df.equals(df_multifamily):
@@ -481,7 +501,7 @@ for df in [df_multifamily,df_office,df_retail,df_industrial]:
     df['Vacancy Rate']                                   = round(df['Vacancy Rate'],2)
     df['Absorption Rate']                                = round(df['Absorption Rate'],2)
 
-
+print('Creating New Variables')
 
 #Keep last 10 years only 
 df_multifamily          =  KeepLast10Years(df_multifamily,groupbylist= ['Geography Name'])
@@ -494,6 +514,7 @@ df_office_slices        =  KeepLast10Years(df_office_slices,groupbylist= ['Geogr
 df_retail_slices        =  KeepLast10Years(df_retail_slices,groupbylist= ['Geography Name','Slice'])
 df_industrial_slices    =  KeepLast10Years(df_industrial_slices,groupbylist= ['Geography Name','Slice'])
 
+print('Keeping 10 Years of Data')
 
 #Export Cleaned Data Files
 df_multifamily.to_csv(os.path.join(costar_data_location,'Clean Data','mf_clean.csv'),index=False)
@@ -505,5 +526,7 @@ df_multifamily_slices.to_csv(os.path.join(costar_data_location,'Clean Data','mf_
 df_office_slices.to_csv(os.path.join(costar_data_location,'Clean Data','office_slices_clean.csv'),index=False)
 df_retail_slices.to_csv(os.path.join(costar_data_location,'Clean Data','retail_slices_clean.csv',),index=False)
 df_industrial_slices.to_csv(os.path.join(costar_data_location,'Clean Data','industrial_slices_clean.csv'),index=False)
+
+print('Exporting Excel Files')
 
 print('Cleaning Complete')
